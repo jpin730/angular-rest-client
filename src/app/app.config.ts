27ea953104d63ed0fdc851519786e72a646b4876
Jaime Pineda <jpin730@pm.me>
2023-09-01 16:34:18 -0600

    chore: :package: add ngrx store and effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()],
+  providers: [provideRouter(routes), provideAnimations(), provideStore(), provideEffects()],
 };
