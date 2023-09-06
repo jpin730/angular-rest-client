ebede51cafbc29d589a52d557cefd53629317e04
Jaime Pineda <jpin730@pm.me>
2023-09-06 17:04:05 -0600

    chore: :wrench: config proxy for cors errors

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 import { Environment } from 'src/app/interfaces/environment';
 
 export const environment: Environment = {
-  apiURL: 'https://jpin730-node-rest-server.onrender.com/api',
-  googleClientId: '290903500448-msd8cbc2mu0bheovou6nui87m9q6ovtc.apps.googleusercontent.com',
+  apiURL: '/api',
+  googleClientId: '1095905799150-t5curj4h3bj5dciu8g61vso2go6vqcn7.apps.googleusercontent.com',
 };
