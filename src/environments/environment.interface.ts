d1183e405d9b7c157ebd8a6bd844c0fcd5bbc0fa
Jaime Pineda <jpin730@pm.me>
2023-09-06 18:07:13 -0600

    refactor: :wrench: refactor environment files

--- src/environments/environment.interface.ts
+++ src/environments/environment.interface.ts
@@ -1,4 +1,3 @@
 export interface Environment {
   apiURL: string;
-  googleClientId: string;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
-import { Environment } from 'src/app/interfaces/environment';
+import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   apiURL: 'https://jpin730-node-rest-server.onrender.com/api',
-  googleClientId: '1095905799150-t5curj4h3bj5dciu8g61vso2go6vqcn7.apps.googleusercontent.com',
 };
