afc4229a5cb930351eb3e74467a29bccca790a50
Jaime Pineda <jpin730@pm.me>
2023-09-07 18:07:59 -0600

    feat: :lipstick: add profile page

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Store } from '@ngrx/store';
+import { fromAuth } from 'src/app/store/auth';
 
 @Component({
   selector: 'app-profile-page',
   standalone: true,
+  imports: [AsyncPipe, NgIf, MatIconModule],
   templateUrl: './profile-page.component.html',
 })
-export class ProfilePageComponent {}
+export class ProfilePageComponent {
+  store = inject(Store);
+  user$ = this.store.select(fromAuth.user);
+}
