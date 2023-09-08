25739cdf02c7ef2431a01202bc40e4943d990f2e
Jaime Pineda <jpin730@pm.me>
2023-09-08 13:38:19 -0600

    feat: :construction: update user avatar

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,6 +1,8 @@
 import { AsyncPipe, NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { Store } from '@ngrx/store';
 import { fromAuth } from 'src/app/store/auth';
 import { ROLE } from 'src/app/utils/constants';
@@ -8,11 +10,28 @@ import { ROLE } from 'src/app/utils/constants';
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [AsyncPipe, NgIf, MatIconModule],
+  imports: [AsyncPipe, NgIf, MatIconModule, MatButtonModule, MatProgressBarModule],
   templateUrl: './profile-page.component.html',
 })
 export class ProfilePageComponent {
   store = inject(Store);
   user$ = this.store.select(fromAuth.user);
   role = ROLE;
+  file?: File;
+  avatarPreview = '';
+
+  submitFile() {
+    const formData = new FormData();
+    formData.append('file', this.file as File);
+  }
+
+  showFilePreview(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const files = target.files as FileList;
+
+    if (files.length) {
+      this.file = files.item(0) as File;
+      this.avatarPreview = URL.createObjectURL(this.file);
+    }
+  }
 }
