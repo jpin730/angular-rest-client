7d539b19c7333411f815b3f85e97771d4f52986b
Jaime Pineda <jpin730@pm.me>
2023-09-08 21:24:01 -0600

    refactor: :truck: move toast styles

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: './tailwind.config.js',
+};
