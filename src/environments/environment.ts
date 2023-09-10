310b596ff5493a9f9ab949caa2cf178f59024f70
Jaime Pineda <jpin730@pm.me>
2023-09-10 00:44:39 -0600

    build: :rocket: update backend host

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { Environment } from './environment.interface';
 
 export const environment: Environment = {
-  apiURL: 'https://node-rest-server-jpin730.vercel.app//api',
+  apiURL: 'https://node-rest-server-jpin730.vercel.app/api',
   googleClientId: '1095905799150-t5curj4h3bj5dciu8g61vso2go6vqcn7.apps.googleusercontent.com',
 };
