9a0a4561f2d581cc0ef741274fd8ecf9fe813524
Jaime Pineda <jpin730@pm.me>
2023-09-10 18:13:20 -0600

    feat: :package: add ngrx router store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
+import { provideRouterStore } from '@ngrx/router-store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideToastr } from 'ngx-toastr';
@@ -23,6 +24,7 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withInterceptors(interceptors)),
     provideAnimations(),
     provideStore(reducers),
+    provideRouterStore({ stateKey: 'router' }),
     provideStoreDevtools(),
     provideEffects(appEffects, authEffects, uploadEffect),
     provideToastr(),
--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -2,13 +2,19 @@ import { ActionReducerMap } from '@ngrx/store';
 
 import { AuthState, authReducer } from './auth/auth.reducer';
 import { UploadState, uploadReducer } from './upload/upload.reducer';
+import { RouterState, routerReducer } from '@ngrx/router-store';
 
 export interface AppState {
+  router: RouterState;
   auth: AuthState;
   upload: UploadState;
 }
 
-export const reducers: ActionReducerMap<AppState> = { auth: authReducer, upload: uploadReducer };
+export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer,
+  auth: authReducer,
+  upload: uploadReducer,
+};
 
 export const selectAuth = (state: AppState) => state.auth;
 export const selectUpload = (state: AppState) => state.upload;
